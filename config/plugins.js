32089db25cf9dbe52dec1eb449ba530d6bd5f6c3
Joey Yax <joey@joeyyax.com>
2022-05-05 14:43:32 -0700

    aws ses

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,35 @@
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
-      jwtSecret: env('JWT_SECRET'),
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
+    },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+  },
+  email: {
+    config: {
+      provider: "amazon-ses",
+      providerOptions: {
+        key: env("AWS_SES_KEY"),
+        secret: env("AWS_SES_SECRET"),
+        amazon: env("AWS_SES_AMAZON"),
+      },
+      settings: {
+        defaultFrom: env("EMAIL_DEFAULT_FROM"),
+        defaultReplyTo: env("EMAIL_DEFAULT_REPLY_TO"),
       },
     },
-  });
+  },
+});
